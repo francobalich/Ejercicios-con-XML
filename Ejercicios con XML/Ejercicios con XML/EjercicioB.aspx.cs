34098ff69beefd1957b1a87e4bd50df30206e708
franco.balich@gmail.com <franco.balich@gmail.com>
2021-07-10 13:36:21 -0300

    Reading of XML file was implemented

--- Ejercicios con XML/Ejercicios con XML/EjercicioB.aspx.cs
+++ Ejercicios con XML/Ejercicios con XML/EjercicioB.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace Ejercicios_con_XML
 {
@@ -11,7 +13,33 @@ namespace Ejercicios_con_XML
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
+            string path = Server.MapPath("Sueldos.xml");
+            if (File.Exists(path))
+            {
+                XmlTextReader archivoXML = new XmlTextReader(path);
+                Dictionary<string,string> datos = new Dictionary<string, string>();
+                while (archivoXML.Read())
+                {
+                    if (archivoXML.Name=="Apellido")
+                    {
+                        archivoXML.Read();
+                        lblApellido.Text = archivoXML.Value;
+                        archivoXML.Read();
+                    }
+                    else if (archivoXML.Name == "Sueldo")
+                    {
+                        archivoXML.Read();
+                        lblSueldo.Text = archivoXML.Value;
+                        archivoXML.Read();
+                    }
+                }
+                archivoXML.Close();
+            }
+            else
+            {
+                lblMsg.Attributes.Add("style", "opacity:1;");
+            }
         }
     }
 }
